f2258025df89098ebc61f12d1cec0bf2886db002
Fatih Öner <fatihhonr@gmail.com>
2019-02-22 22:50:50 +0300

    proje 2 dogruluk 98

--- Project2_TrafficSign/traffic_sign.py
+++ Project2_TrafficSign/traffic_sign.py
@@ -1,197 +1,241 @@
 import pickle
 
-training_file   =   'data/train.p'
-validation_file =   'data/validate.p'
-testing_file    =   'data/test.p'
+training_file = 'data/train.p'
+validation_file = 'data/validate.p'
+testing_file = 'data/test.p'
 
 with open(training_file, mode='rb') as f:
-  train = pickle.load(f, encoding='latin1')
-
+    train = pickle.load(f,encoding='latin1')
 with open(validation_file, mode='rb') as f:
-  valid = pickle.load(f, encoding='latin1')
-
+    valid = pickle.load(f, encoding='latin1')
 with open(testing_file, mode='rb') as f:
-  test = pickle.load(f, encoding='latin1')
+    test = pickle.load(f, encoding='latin1')
 
+    
+    
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
-X_test, y_test   = test['features'], test['labels']
+X_test, y_test = test['features'], test['labels']
 
 import numpy as np
+
 X_train = np.array(X_train)
 X_valid = np.array(X_valid)
 X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_valid = np.array(y_valid)
+y_test = np.array(y_test)
 
+X_train = np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
+X_valid = np.pad(X_valid, ((0,0),(2,2),(2,2),(0,0)),'constant')
+X_test = np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)), 'constant')
 
-n_train = X_train.shape[0] 
-n_validation= X_valid.shape[0]
+n_train = X_train.shape[0]
 n_test = X_test.shape[0]
-image_shape = X_train.shape[1:] 
-n_classes = len(np.unique(y_train)) 
-print("Number of training examples =", n_train)
-print("Number of testing examples =", n_test)
-print("Image data shape =", image_shape)
-print("Number of classes =", n_classes)
+image_shape = X_train.shape[1:]
+n_classes = len(np.unique(y_train))
 
-import matplotlib.pyplot as plt
-#plt.imshow(X_train[0], cmap='gray')
-#plt.show()
-
-# 28x28 to 32x32
-X_train= np.pad(X_train, ((0,0),(2,2),(2,2),(0,0)), 'constant')
-X_test= np.pad(X_test, ((0,0),(2,2),(2,2),(0,0)),'constant')
-X_valid= np.pad(X_valid, ((0,0),(2,2),(2,2),(0,0)),'constant')
-
-#TODO GRAYSCALE DONUSTUR
-# GrayScale Donusum islemleri bu sekildedir.
-X_train = np.sum(X_train/3, axis=3, keepdims=True)
-X_test = np.sum(X_test/3, axis=3, keepdims=True)
-X_valid = np.sum(X_valid/3, axis=3, keepdims=True)
-#print('Converted to grayscale, the shape is : ', X_train.shape)
+print("Number of training examples ", n_train)
+print("Number of testing examples ", n_test)
 
 
-#TODO VERI SETINI NORMALIZE ET
-# Normalize islemi
-X_train_normalized =  (X_train - 128)  / 128
-#x_valid =  (X_valid - 128)  / 128
-X_test_normalized =   (X_test - 128)   / 128
+### Preprocess images: normalize images from [0,255] to [0,1], and gray scale it
+### Rationale: although colors in the traffic sign are important in real world for people to recoganize
+###            different signs, traffic signs are also different in their shapes and contents. We can
+###            ignore colors in this problem because signs in our training set are differentiable from
+###            their contents and shapes.
+import numpy as np
+import cv2
 
-#plt.imshow(X_train_normalized[0].squeeze(), cmap='gray')
-#plt.show()
-
-#TODO - RANDOM_TRANSLATE
-
-#TODO - RANDOM_SCALING
-
-#TODO - RANDOM_WARP
-
-#TODO - RANDOM_BRIGHTNESS
-
-
-
-
-
-
-#TODO VERI SETINI KARISTIR
-# Veri Setini Karistirma Islemi
-from sklearn.utils import shuffle
-X_train, y_train = shuffle(X_train, y_train)
-
-
-#TODO LENET MODELINI KUR
-import tensorflow as tf
-EPOCHS = 10
-BATCH_SIZE = 128
-
-from tensorflow.contrib.layers import flatten
-
-def LeNet(x):
-  mu = 0
-  sigma = 0.1
-  
-  # NOT: x_shape [2,3] diyelim ve 1 kanal
-  # NOT: valid_pad = maxpool 2x2 kernel, 2-stride, VALID padding->shape[1,1]
-  # NOT: same_pad = maxpool 2x2 kernel, 2-stride, SAME padding->shape[2,4]
-
-  # Layer 1 ---------- 3->RGB 1->GRAY
-  conv1_W = tf.Variable(tf.truncated_normal(shape=(5,5,1,6),mean=mu,stddev=sigma))
-  conv1_b = tf.Variable(tf.zeros(6))
-  conv1 = tf.nn.conv2d(x, conv1_W, strides=[1,1,1,1], padding='VALID') + conv1_b
-  # RELU- nonlinear function
-  conv1 = tf.nn.relu(conv1)
-  # MAX POOL input = 28x28x6 output=14x14x6
-  conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-
-
-  # Layer 2 ----------
-  conv2_W = tf.Variable(tf.truncated_normal(shape=(5,5,6,16),mean=mu,stddev=sigma))
-  conv2_b = tf.Variable(tf.zeros(16))
-  conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1,1,1,1],padding='VALID')+conv2_b
-  # RELU
-  conv2 = tf.nn.relu(conv2)
-  # MAX POOL input = 10x10x16 output=5x5x16
-  conv2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-  
-
-  # FLATTEN input = 5x5x16 output=400
-  fc0 = flatten(conv2)
-
-  # Layer 3: Fully Connected Layer Input=400. Output=120
-  fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120),mean=mu,stddev=sigma))
-  fc1_b = tf.Variable(tf.zeros(120))
-  fc1 = tf.matmul(fc0, fc1_W)+fc1_b
-
-  # Activation RELu
-  fc1 = tf.nn.relu(fc1)
-  
-  # Dropout
-  fc1 = tf.nn.dropout(fc1, 0.7)
-
-  # Layer 4: Fully Connected Layer Input=120. Output=84
-  fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu,stddev=sigma))
-  fc2_b = tf.Variable(tf.zeros(84))
-  fc2 = tf.matmul(fc1, fc2_W) + fc2_b
-  #Activation RELu
-  fc2 = tf.nn.relu(fc2)
-  
-  # Dropout
-  fc2 = tf.nn.dropout(fc2, 0.7)
-  
-  # Layer 5: Fully Connected : Input = 84, output=43
-  fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=mu, stddev=sigma))
-  fc3_b = tf.Variable(tf.zeros(43))
-  logits = tf.matmul(fc2, fc3_W) + fc3_b
-  
-  return logits
-  
-x = tf.placeholder(tf.float32, (None, 32, 32, 1)) 
-y = tf.placeholder(tf.int32, (None))
-one_hot_y = tf.one_hot(y, 43)
-
-rate = 0.001
-logits = LeNet(x)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate = rate)
-training_operation = optimizer.minimize(loss_operation)
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-def evaluate(X_data, y_data):
-  num_examples = len(X_data)
-  total_accuracy = 0 
-  ses = tf.get_default_session()
-  for offset in range(0, num_examples, BATCH_SIZE):
-    batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+def reshape_raw_images(imgs):
+    """Given 4D images (number, heigh, weight, channel), this
+    function grayscales and returns (number, height, weight, 1) images"""
+    def gray(src):
+        if src.dtype == np.uint8:
+            src = np.array(src/255.0, dtype=np.float32)
+        dst = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+        return dst.reshape(32,32,1)
+    norms = [gray(img) for img in imgs]
+    return np.array(norms)
     
-    accuracy = ses.run(accuracy_operation, feed_dict={x:batch_x, y:batch_y})
-    total_accuracy += (accuracy*len(batch_x))
-    return total_accuracy/num_examples
+X_train = reshape_raw_images(X_train)
+labels_train   = y_train
+
+X_valid = reshape_raw_images(X_valid)
+labels_valid   = y_valid
+
+X_test  = reshape_raw_images(X_test)
+labels_test    = y_test
+
+### Architecture:
+###   I adapted LeNet architecture: Two convolutional layers followed by one flatten layer and three
+###   fully connected linear layers.
+###
+###   convolution 1: 32x32x1  -> 28x28x12 -> relu -> 14x14x12 (pooling)
+###   convolution 2: 14x14x12 -> 10x10x25 -> relu -> 5x5x25   (pooling)
+###         flatten: 5x5x25   -> 625
+###        drop out: 625      -> 625
+###          linear: 625      -> 300
+###          linear: 300      -> 150
+###          linear: 150      -> 43
+
+### Experiment shows that
+### 1. drop out has positive impact on the accuracy
+### 2. longer depth (to some extent) on convolution filter has better accuracy
+
+import tensorflow as tf
+
+mu, sigma = 0, 0.1
+
+def conv(input, in_len, in_depth, out_len, out_depth):
+    """ Define a convolutional network layer
+    @param input: input data or model
+    @param in_len: 0D, the input height and width (assume they're the same)
+    @param in_depth: 0D, the input depth (e.g. 3 for RGB images)
+    @param out_len: 0D, desired output height and width
+    @param out_depth: 0D, desired output depth
+    """
+    filter_len = in_len - out_len + 1
+    # we're not going to use stride to reduce the dimention,
+    # we're going to use pooling instead.
+    strides = [1,1,1,1]
+
+    W = tf.Variable(tf.truncated_normal(shape=(filter_len, filter_len, in_depth, out_depth), \
+                                        mean = mu, stddev = sigma))
+    b = tf.Variable(tf.zeros(out_depth))
+    model = tf.nn.conv2d(input, W, strides=strides, padding='VALID') + b
+    return model
+
+def linear(input, in_size, out_size):
+    W = tf.Variable(tf.truncated_normal(shape=(in_size, out_size), \
+                                        mean=mu, stddev = sigma))
+    b = tf.Variable(tf.zeros(out_size))
+    model = tf.matmul(input, W) + b
+    return model
+    
+def classifier(input, keep_prob):
+    ## Layer1
+    # convolution layer: 32x32x1 -> 28x28x12
+    conv1 = conv(input, 32, 1, 28, 12)
+    conv1 = tf.nn.relu(conv1)
+    # pooling: 28x28x12 -> 14x14x12
+    conv1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+    
+    ## Layer2
+    # convolution layer 14x14x12 -> 10x10x25
+    conv2 = conv(conv1, 14, 12, 10, 25)
+    conv2 = tf.nn.relu(conv2)
+    # pooling: 10x10x25 -> 5x5x25
+    conv2 = tf.nn.max_pool(conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
+    
+    ## Layer3
+    # -> 625
+    flat = tf.contrib.layers.flatten(conv2)
+    dropped = tf.nn.dropout(flat, keep_prob)
+    layer3 = linear(dropped, 625, 300)
+    layer3 = tf.nn.relu(layer3)
+    
+    ## Layer4
+    layer4 = linear(layer3, 300, 100)
+    layer4 = tf.nn.relu(layer4)
+    
+    ## Layer 5
+    layer5 = linear(layer4, 100, n_classes)
+    return layer5
+
+### Define the model and its input
+
+from sklearn.utils import shuffle
+
+feature_shape = X_train.shape[1:]
+x = tf.placeholder(tf.float32, shape=(None,)+feature_shape)
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, n_classes)
+# drop out layer parameter. This parameter should be 1.0 when evaluate and test
+# the model, less than 1.0 when training
+keep_prob = tf.placeholder(tf.float32)
+
+logits = classifier(x, keep_prob)
+
+# A function for evaluating the accuracy of a model
+prediction_step = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuary_step = tf.reduce_mean(tf.cast(prediction_step, tf.float32))
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, batch_size):
+        batch_x, batch_y = X_data[offset:offset+batch_size], y_data[offset:offset+batch_size]
+        accuracy = sess.run(accuary_step, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+import time
+
+saver = tf.train.Saver()
+model_file = './model/model'
+
+epoch=10
+batch_size = 64
+rate = 0.001
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
+loss = tf.reduce_mean(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate = rate).minimize(loss)
+
+X_train, y_train = shuffle(X_train, y_train)
+def train(X_data, y_data): 
+  with tf.Session() as ses:
+    ses.run(tf.global_variables_initializer())
+    print('Starting...')
+    for i in range(epoch):
+      begin_time = time.time()
+
+      for offset in range(0, len(X_train), batch_size):
+        end= offset+batch_size
+
+        features, labels = X_train[offset:end], y_train[offset:end]
+        ses.run(train_step, feed_dict={x:features, y:labels, keep_prob:0.8})
+      validation = evaluate(X_valid, y_valid)
+      print("[{3:.1f}s] epoch {0}/{1}: validation = {2:.3f}".format(i+1, epoch, validation, time.time()-begin_time))
+
+      saver.save(ses, model_file)
+      print('Model saved')
+
+train(X_train, y_train)
+
 
 with tf.Session() as ses:
-  ses.run(tf.global_variables_initializer())
-  num_examples = len(X_train)
+  saver.restore(ses, model_file)
+  acc = evaluate(X_test, y_test)
+  print('Accuracy is {}'.format(acc))
 
-  print("Training")
-  print()
 
-  for i in range(EPOCHS):
-    X_train, y_train = shuffle(X_train, y_train)
-    
-    for offset in range(0, num_examples, BATCH_SIZE):
-      end=offset + BATCH_SIZE
-      batch_x , batch_y = X_train[offset:end], y_train[offset:end]
-      ses.run(training_operation, feed_dict={x:batch_x, y:batch_y})
+"""
+from PIL import Image
+import os
 
-    validation_accuracy = evaluate(X_valid, y_valid)
-    print('EPOCH {} ...'.format(i+1))
-    print('Validation accuracy = {:.3f}'.format(validation_accuracy))
-    print()
+def read_file_to_32x32_array(file):
+    x = Image.open(file).convert("RGB")
+    x = x.resize((32,32))
+    return np.array(x)
 
-  try:
-    saver
-  except NameError:
-    saver = tf.train.Saver()
-  saver.save(ses, './lenet_traffic')
-  print('Model Saved')
 
+def predict(features, human_readable=False):
+    with tf.Session() as sess:
+        saver.restore(sess, model_file)
+        results = sess.run(tf.argmax(logits, 1), {x : features, keep_prob:1.0})
+        if human_readable:
+            results = [signname[n] for n in results]
+        return results
+
+
+while True:
+  yol = input('Resim yolu giriniz... ')
+	my_images = np.array([read_file_to_32x32_array(yol)])
+	my_features = reshape_raw_images(my_images)
+	plt.plot(my_images)
+
+	predict(my_feature, True)  
+"""
